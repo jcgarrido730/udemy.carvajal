6ed1940cddc7be64c8220ebb943a4dfd319bc1d6
Juan Carlos Garrido Sarasa <jcgarrido@gmail.com>
2022-05-14 14:25:04 -0500

    continuación ejercicio 03-functions.ts

--- Principios_SOLID_y_Clean_Code/CleanSolid/src/clean-code/03-functions.ts
+++ Principios_SOLID_y_Clean_Code/CleanSolid/src/clean-code/03-functions.ts
@@ -36,7 +36,25 @@
 
     }
 
-    
+    //continuación
+    const getPayAmount = ({ isDead = false, isSeparated = true, isRetired = false }) => {
+        let result;
+        if ( isDead ) {
+            result = 1500;
+        } else {
+            if ( isSeparated ) {
+                result = 2500;
+            } else {
+                if ( isRetired ) {
+                    result = 3000;
+                } else {
+                    result = 4000; 
+                }
+            }
+        }
+        
+        return result;
+    }
 
 
 })();
