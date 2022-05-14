ab233292349cf0a438c168a10288b49e02d5e6d5
Juan Carlos Garrido Sarasa <jcgarrido@gmail.com>
2022-05-14 14:18:25 -0500

    solución ejercio 03-functions.ts

--- Principios_SOLID_y_Clean_Code/CleanSolid/src/clean-code/03-functions.ts
+++ Principios_SOLID_y_Clean_Code/CleanSolid/src/clean-code/03-functions.ts
@@ -1,34 +1,37 @@
 (() => {
 
-    // función para obtener información de una película por Id
-    function getAllMovies( movieId: string ) {
+    function getMovieById( movieId: string ) {
         console.log({ movieId });
     }
 
-    // función para obtener información de los actores de una película - Actors o Cast // id = movieId getMovieCast
-    function getAllMovieActors( id: string ) {
+    function getMovieCastById( id: string ) {
         console.log({ id });
     }
 
-    // funcion para obtener el bio del actor por el id
-    function getUsuario( ActorId: string ) {
+    function getActorBioById( ActorId: string ) {
         console.log({ ActorId });
     }
     
-    // Crear una película
-    function movie(title: string, description: string, rating: number, cast: string[] ) {
+    interface Movie{
+        cast:        string[]
+        description: string;
+        rating:      number;
+        title:       string;
+    }
+    function createMovie({title, description, rating, cast } : Movie) {
         console.log({ title, description, rating, cast });
     }
 
-    // Crea un nuevo actor
-    function createActorIfActorNotExists( fullName: string, birthdate: Date ): boolean {
-        
-        // tarea asincrona para verificar nombre
-        // ..
-        // ..
-        if ( fullName === 'fernando' ) return false;
+    function checkFullName(fullName: string){
+        console.log({fullName});
+        return true;
+    }
 
-        console.log('Crear actor');
+    function createActor( fullName: string, birthdate: Date ): boolean {
+        
+        if ( checkFullName(fullName) ) return false;
+
+        console.log('Crear actor', birthdate);
         return true;        
 
     }
