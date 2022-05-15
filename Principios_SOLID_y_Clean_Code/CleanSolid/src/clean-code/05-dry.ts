1a7fb98356a49b17273fa3fa74d641645d34b1b2
Juan Carlos Garrido Sarasa <jcgarrido@gmail.com>
2022-05-15 00:37:33 -0500

    aplicando dry ejercicio 05-dry.ts

--- Principios_SOLID_y_Clean_Code/CleanSolid/src/clean-code/05-dry.ts
+++ Principios_SOLID_y_Clean_Code/CleanSolid/src/clean-code/05-dry.ts
@@ -9,12 +9,26 @@ class Product{
         public size: Size = '',
     ){}
 
-    toString(){
-        //No Dry
-        if(this.name.length <= 0) throw Error('the name is empty');
-        if(this.price <= 0) throw Error('the price is zero');
-        if(this.size.length <= 0) throw Error('the size is empty');
+    isProductReady(){
+        for(const key in this){
+            switch(typeof this[key]){
+                case 'string':
+                    if((<string><unknown>this[key]).length <= 0) throw Error(`${key} is empty`);
+                break;
+                case 'number':
+                    if((<number><unknown>this[key]) <= 0) throw Error(`${key} is zero`);
+                break;
 
+                default:
+                    throw Error(`${typeof this[key]} is not valid`);
+            }
+        }
+        return true;
+    }
+
+    toString(){
+        if(!this.isProductReady) return;
+        
         return `${this.name} (${this.price}), ${this.size}`
     }
 }
