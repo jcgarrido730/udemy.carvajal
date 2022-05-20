a7a885ba2ea8f19072082693a0d3dcc12537424f
Juan Carlos Garrido Sarasa <jcgarrido@gmail.com>
2022-05-19 23:01:07 -0500

    No aplicando el principio de responsabilidad única 06-classes-a.ts

--- Principios_SOLID_y_Clean_Code/CleanSolid/src/clean-code/06-classes-a.ts
+++ Principios_SOLID_y_Clean_Code/CleanSolid/src/clean-code/06-classes-a.ts
@@ -1,29 +1,58 @@
 //npm run dev
 (()=>{
-
+//No aplicando el principio de responsabilidad única
     type Gender = 'M'|'F';
-    /*
-    class Person{
-        public name: string;
-        public gender: Gender;
-        public birthdate: Date;
 
-        constructor(name: string, gender: Gender, birthdate: Date){
-            this.name = name;
-            this.gender = gender;
-            this.birthdate = birthdate;
-        }
-    */
-
-    /*Forma corta de manejar las clases en typescript */
     class Person{
         constructor(
             public name: string,
             public gender: Gender,
             public birthdate: Date
         ){}
-}
+    }
 
-const newPerson = new Person('Fulano', 'M', new Date('1900-01-01'));
-console.log({newPerson})
+    class User extends Person{
+        public lastAccess : Date;
+
+        constructor(
+            public email : string,
+            public role : string,
+            name : string,
+            gender : Gender,
+            birthdate : Date,
+        ){
+            super(name, gender, birthdate);
+            this.lastAccess = new Date();
+        }
+
+        checkCredentials(){
+            return true;
+        }
+    }
+
+    class UserSettings extends User{
+        constructor(
+            public workingDirectory : String,
+            public lastOpenFolder : string,
+            email : string,
+            role : string,
+            name : string,
+            gender : Gender,
+            birthdate : Date
+        ){
+            super(email, role, name, gender, birthdate);
+        }
+    }
+
+    const userSettings = new UserSettings(
+        '/home/Fulano',
+        '/home/Fulano',
+        'Fulano@gmail.com',
+        'Admin',
+        'Fulano',
+        'M',
+        new Date('1900-01-01')
+    );
+    
+    console.log({userSettings, areCredentialsValid: userSettings.checkCredentials()});
 })();
