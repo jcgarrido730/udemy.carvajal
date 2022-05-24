7b9cd2b0c26a0885afd98cf4008abc179e075600
Juan Carlos Garrido Sarasa <jcgarrido@gmail.com>
2022-05-23 22:13:47 -0500

    SOLID SRP: aplicando el principio de responsabilidad única. 01-srp.ts

--- Principios_SOLID_y_Clean_Code/CleanSolid/src/main.ts
+++ Principios_SOLID_y_Clean_Code/CleanSolid/src/main.ts
@@ -7,7 +7,8 @@ import './style.css'
 //import './clean-code/06-classes-c'
 //import './clean-code/07-tarea'
 //import './code-smells/02-high-coupling'
-import './code-smells/02-low-coupling'
+//import './code-smells/02-low-coupling'
+import './solid/01-srp'
 
 const app = document.querySelector<HTMLDivElement>('#app')!
 
